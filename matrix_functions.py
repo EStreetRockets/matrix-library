fd452aa5ef4036e2250e8c41ec068dbe455e1436
gaussian-dev <43509132+gaussian-dev@users.noreply.github.com>
2018-10-01 23:19:22 -0400

    Add files via upload

--- matrix_functions.py
+++ matrix_functions.py
@@ -38,3 +38,52 @@ def spaced_matrix(beg,end,space):
         mtrx.append(i)
         i += step
     return matrix([mtrx])
+
+def matrix_solve(expressions, equals, variables):
+    """
+    Solves a system of equations put into Ax + By + ... Cz = D form
+    All variables must be in the same order and all expression and variables must be strings.
+    The solutions should be a float or int, and the variables must be single-character
+    Returns the solutions in order of variables parameter
+    """
+    expressions = list(expressions)
+    equals = list(equals)
+    variables = list(variables)
+    new_exprs = []
+    for i, expr in enumerate(expressions):
+        expr=expr.replace(" ","")
+        expressions[i] = expr
+        terms = []
+        pre_index = 0
+        for var in variables:
+            index = expr.index(var)
+            terms.append(expr[pre_index:index+1])
+            pre_index = index+1
+        new_exprs.append(terms)
+
+    coefficients = []
+    for expression in new_exprs:
+        expr_coeff = []
+        for term in expression:
+            for var in variables:
+                if var in term:
+                    num = term.replace(var,"")
+                if  num == '' or  num == '+':
+                    num = 1
+                elif num == '-':
+                    num = -1
+            num = float(num)
+            expr_coeff.append(num)
+        coefficients.append(expr_coeff)
+    
+    matrixForm = matrix(coefficients)
+    equalsMatrix = []
+    for a in equals:
+        equalsMatrix.append([a])
+    equalsMatrix = matrix(equalsMatrix)
+    solutions = matrixForm.inv().dot(equalsMatrix)
+    final = []
+    for sol in solutions:
+        final.append(sol[0])
+    
+    return tuple(final)
